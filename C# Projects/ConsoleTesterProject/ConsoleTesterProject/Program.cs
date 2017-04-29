5bf9c8e45da7806a9b002fabb00bf1054cf999b0
David Knieradl <fd45ea11@gmail.com>
2017-04-29 21:50:44 +0200

    still doesn't work huh ᕕ(◔ ͜つ◔)ᕗ

--- C# Projects/ConsoleTesterProject/ConsoleTesterProject/Program.cs
+++ C# Projects/ConsoleTesterProject/ConsoleTesterProject/Program.cs
@@ -10,21 +10,70 @@ using System.Text;
 
 namespace ConsoleTesterProject
 {
-    class Foo
-    { }
+    public class Parabola
+    {
+        public TimeSpan Time;
+        public float Y;
 
-    class Bar : Foo
-    { }
+        public float Accumulated { get; private set; }
+
+        public bool Enabled = false;
+
+        float Elapsed;
+
+        float[] ValuesInTime;
+        int NumberOfSamples;
+        const float SamplesInSecodnd = 1000;
+
+        public Parabola(int Y, TimeSpan T)
+        {
+            Time = T; this.Y = Y;
+
+            NumberOfSamples = (int)Time.TotalMilliseconds;
+            ValuesInTime = new float[NumberOfSamples];
+
+            double midpoint = NumberOfSamples / 2;
+            double start = -midpoint;
+
+            for (int i = (int)start; i < (int)midpoint; i++)
+            {
+                ValuesInTime[(int)(i + midpoint)] = (-(float)(Math.Pow(1 / midpoint * i, 2)) + Y);
+                /*float result = (float)(Math.Sin(i * (Math.PI / SamplesInSecodnd)) * Y);
+                ValuesInTime[i] = i > 0 ? (float)(Math.Sin(i * (Math.PI / SamplesInSecodnd)) * Y) - ValuesInTime[i - 1] : 0;*/
+            }
+
+            //Elapsed += Engine.DeltaTime;
+        }
+
+        protected void Update(float miliseconds)
+        {
+            Accumulated = 0;
+            float MaxTime = 0;
+            float LeftoverTime = (float)((Elapsed + miliseconds) - Time.TotalMilliseconds);
+
+            if (LeftoverTime > 0)
+            {
+                MaxTime = (float)(Time.TotalMilliseconds - LeftoverTime);
+            }
+            else
+            {
+                MaxTime = Elapsed + miliseconds;
+            }
+
+            for (int i = (int)Elapsed; i <= (int)MaxTime; i++)
+            {
+                Accumulated += ValuesInTime[i];
+            }
+
+            Elapsed += miliseconds;
+        }
+    }
 
     static class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<int, Foo> dictionar = new Dictionary<int, Foo>();
-
-            dictionar.Add(0, (Foo)new Bar());
-
-            Console.WriteLine(dictionar[0].GetType() == typeof(Bar));
+            Parabola x = new Parabola(10, new TimeSpan(0, 0, 1));
 
             Console.ReadLine();
         }
